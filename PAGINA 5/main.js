58102c8f2cee1aab3effe34778875f5dffcf17da
Mariana <mtelechea@udesa.edu.ar>
2019-11-19 06:48:19 -0300

    Botón favorito

--- PAGINA 5/main.js
+++ PAGINA 5/main.js
@@ -118,7 +118,62 @@ fetch("https://api.themoviedb.org/3/tv/" + idRecom +"/similar?api_key=65eadee9d6
              }
         })
 
+    // "<li><a href='../PAGINA 5/descripcion.html?id=" + series.results[i].id + "'><img src='https://image.tmdb.org/t/p/original" + series.results[i].backdrop_path + "'></a></li>"
 
 
 
-        // "<li><a href='../PAGINA 5/descripcion.html?id=" + series.results[i].id + "'><img src='https://image.tmdb.org/t/p/original" + series.results[i].backdrop_path + "'></a></li>"
+  // API DE OS FAVORITOS //
+
+  //Paso 1: Leo Storage
+
+var recuperoStorage = localStorage.getItem("seriesFavoritas");
+
+// Si todavía no tenía gifs favoritos
+if (recuperoStorage == null) {
+  // Creo una lista vacia
+  seriesFavoritas = [];
+} else {
+  // Descomprimo el TEXTO que tenia en storage en el array que necesito trabajar
+  seriesFavoritas = JSON.parse(recuperoStorage);
+}
+
+var datos = new URLSearchParams(location.search);
+var idSerie = datos.get("idSerie");
+
+if (seriesFavoritas.includes(idSerie)) {
+  document.querySelector("#favoritos").innerHTML = "QUITAR DE FAVORITOS";
+}
+
+
+
+fetch("https://api.themoviedb.org/3/tv/" + seriesFavoritas + "?api_key=65eadee9d6749b2ab92f01099d10deeb&language=en-US&page=1")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(serie) {
+    document.querySelector("h1").innerHTML = serie.data.title;
+    document.querySelector("img").src = serie.data.images.original.url;
+  })
+
+  document.querySelector("button").onclick = function() {
+
+
+    //Paso 2: Modificar la informacion
+    // Si el gif ya era favorito
+    if (seriesFavoritas.includes(idSerie)) {
+      // Lo quito
+      var index = seriesFavoritas.indexOf(idSerie);
+      seriesFavoritas.splice(index, 1);
+      document.querySelector("#favoritos").innerHTML = "AGREGAR FAVORITO";
+    } else {
+      //Lo agrego
+      seriesFavoritas.push(idSerie);
+      document.querySelector("#favoritos").innerHTML = "QUITAR DE FAVORITOS";
+    }
+
+
+    //Paso 3: Escribir en storage
+    var infoParaStorage = JSON.stringify(seriesFavoritas);
+    localStorage.setItem("seriesFavoritas", infoParaStorage);
+    console.log(localStorage);
+  }
